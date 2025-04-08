21dda6363ae8fa86e59ff81dc7c013fce3eb54cb
siddheshSutar <siddheshsutar96@gmail.com>
2025-04-08 19:42:47 +0530

    slider 2 installed

--- client/src/components/sections/testimonials.tsx
+++ client/src/components/sections/testimonials.tsx
@@ -3,6 +3,12 @@ import { Star } from "lucide-react";
 import Title from "../common/Title";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+// import Slider, { Settings } from "react-slick";
+
+// //@ts-ignore
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
@@ -22,6 +28,40 @@ const responsive = {
     items: 1
   }
 };
+// const settings: Settings = {
+//   dots: true,
+//   infinite: true,
+//   speed: 500,
+//   slidesToShow: 3,
+//   slidesToScroll: 3,
+//   centerPadding: '30px',
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2,
+//         initialSlide: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//   ]
+// };
 
 const testimonials = [
   {
@@ -90,6 +130,7 @@ const Testimonials = () => {
             // dotListClass="custom-dot-list-style"
             // itemClass="carousel-item-padding-40-px"
           >
+           {/* <Slider {...settings}> */}
             {testimonials.map((testimonial, index) => (
 
               <Card key={index} className="bg-white shrink-0 shadow-md w-[400px]">
@@ -128,6 +169,7 @@ const Testimonials = () => {
               </Card>
             ))}
           </Carousel>
+          {/* </Slider> */}
         </div>
       </div>
     </section>
